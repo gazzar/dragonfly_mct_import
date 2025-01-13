e2050a168b2aeb6ac1a136a9fbb1b8f0d172e0e2
Gary Ruben <rubeng@ansto.gov.au>
2025-01-13 15:11:16 +1100

    Updates to get the import working in all current Dragonfly versions

--- MenuItemImportMctRecon_fabaf8b8b65611eebed60242ac150006.py
+++ MenuItemImportMctRecon_fabaf8b8b65611eebed60242ac150006.py
@@ -1,18 +1,29 @@
 """
 An importer for volumetric CT data reconstructed with the Australian Synchrotron's MCT beamline reconstruction pipeline.
 
+2025-01-13 Update (Gary Ruben)
+- This version should work in all Dragonfly versions
+- Thanks to Elvis Stansvik for a code improvement to path checking
+
+2024-04-24 Update (Gary Ruben)
+- Changed import to use either Qt5 or Qt6 to allow operation in Dragonfly 2022 or 2024
+
+2024-04-23 Update (Hubert Taieb, Dragonfly Team)
+- Fix for Qt6, need to have directory as argument, not dir
+- Added a support to have the last folder used
+
 :author: Gary Ruben
 :contact: 
 :email: rubeng@ansto.gov.au
 :organization: Australian Synchrotron
 :address: 800 Blackburn Rd, Clayton VIC 3168, Australia
 :copyright: 
-:date: Jan 18 2024 15:39
-:dragonflyVersion: 2022.2.0.12227
+:date: Apr 22 2024
+:dragonflyVersion: 2022.2 or 2024.1
 :UUID: fabaf8b8b65611eebed60242ac150006
 """
 
-__version__ = '1.0.0'
+__version__ = '2.0.3'
 
 import sys
 from pathlib import Path
@@ -20,11 +31,21 @@ from pathlib import Path
 import numpy as np
 import h5py
 
-from PyQt5.QtWidgets import QFileDialog, QMessageBox
+try:
+    from PyQt5.QtCore import QFileInfo
+    from PyQt5.QtWidgets import QFileDialog, QMessageBox
+except ModuleNotFoundError:
+    from PyQt6.QtCore import QFileInfo
+    from PyQt6.QtWidgets import QFileDialog, QMessageBox
+
 from ORSServiceClass.menuItems.userDefinedMenuItem import UserDefinedMenuItem
 from ORSServiceClass.actionAndMenu.menu import Menu
 from ORSServiceClass.decorators.infrastructure import interfaceMethod
 from ORSModel import createChannelFromNumpyArray
+from OrsLibraries.savedstate import SavedState
+from OrsLibraries.workingcontext import WorkingContext
+
+
 # from ORSModel.ors import DimensionUnit
 
 
@@ -38,7 +59,7 @@ def load_file(filepath: str, h5_path: str) -> None:
     h5_path : str
         Path in hdf5 file to the data volume (case sensitive), e.g. /data
     """
-    
+
     # The Dragonfly method createChannelFromNumpyArray accepts only these Numpy data types:
     # np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32 and np.float32
 
@@ -79,12 +100,8 @@ def h5_path_exists(filepath: str, h5_path: str) -> bool:
     bool
         True iff the path exists
     """
-    try:
-        with h5py.File(filepath, 'r') as f:
-            _data = f[h5_path]
-        return True
-    except KeyError:
-        return False
+    with h5py.File(filepath, 'r') as f:
+        return h5_path in f
 
 
 def error_dialog(title: str, text: str) -> None:
@@ -118,7 +135,6 @@ class MenuItemImportMctRecon_fabaf8b8b65611eebed60242ac150006(UserDefinedMenuIte
         """
         return 'MCT'
 
-
     @classmethod
     def getMenuItem(cls):
         """
@@ -130,19 +146,24 @@ class MenuItemImportMctRecon_fabaf8b8b65611eebed60242ac150006(UserDefinedMenuIte
                          action='MenuItemImportMctRecon_fabaf8b8b65611eebed60242ac150006.menuItemEntryPoint()')
         return aMenuItem
 
-
     @classmethod
     def menuItemEntryPoint(cls):
         """
         Will be executed when the menu item is selected.
         """
-        filepath, _filter = QFileDialog.getOpenFileName(
-            caption="Please select an hdf5 recon file",
-            directory="/data/mct",
-            filter="hdf5 (*.h5 *.hdf5)",
-        )
+        folder = SavedState.getStringStateData("ExportImportFolder", "")
+        if folder == "/":
+            folder = "/data/mct"
+        filepath, _filter = QFileDialog.getOpenFileName(WorkingContext.getCurrentContextWindow(),
+                                                        caption="Please select an hdf5 recon file",
+                                                        directory=folder,
+                                                        filter="hdf5 (*.h5 *.hdf5)",
+                                                        )
 
         if filepath is not None:
+            # Save folder for next time
+            fi = QFileInfo(filepath[0])
+            SavedState.setStringStateData('ExportImportFolder', fi.absolutePath())
             # MCT recon files contain the reconstructed volume data in a couple of locations
             # Try to read from each in turn
             if h5_path_exists(filepath, h5_path := "/data"):
